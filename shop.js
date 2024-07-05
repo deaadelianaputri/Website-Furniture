0db31efb69268f1cca8f3204222c9a0c247c45c0
deaadelianaputri <deaadelianaputri@students.amikom.ac.id>
2024-07-05 18:58:17 +0700

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,318 @@
+const allProductsData = {
+    "products": [
+        {
+            "category": "lemari",
+            "title": "Lemari Hias",
+            "price": "Rp 800.000",
+            "image": "asetshop/lemarihias.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "springbed",
+            "title": "Springbed Innerspring",
+            "price": "Rp 14.000.000",
+            "image": "asetshop/Product2.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 4,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "meja",
+            "title": "Meja Rias Chic Charm",
+            "price": "Rp 7.500.000",
+            "image": "asetshop/Product3.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "lemari",
+            "title": "Lemari Drawer",
+            "price": "Rp 1.500.000",
+            "image": "asetshop/Product4.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "springbed",
+            "title": "Springbed Pocket Coil",
+            "price": "Rp 18.000.000",
+            "image": "asetshop/Product5.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "lemari",
+            "title": "Lemari Pakaian Nordic",
+            "price": "Rp 3.000.000",
+            "image": "asetshop/Lemaripakaian.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "sofa",
+            "title": "Sofa Serene Comfort",
+            "price": "Rp 18.000.000",
+            "image": "asetshop/sofa1.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "lemari",
+            "title": "Lemari Pakaian Minimalis",
+            "price": "Rp 2.000.000",
+            "image": "asetshop/Lemari3.jpg",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kursi",
+            "image": "asetshop/Kursi1.jpg",
+            "title": "Classic Relaxe",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "price": "Rp 800.000",
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "sofa",
+            "image": "asetshop/sofa2.jpg",
+            "title": "Elegant Lounge Sofa",
+            "price": "Rp 20.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 4,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kitchen",
+            "image": "asetshop/Dapur1.jpg",
+            "price": "Rp 28.000.000",
+            "title": "Kitchen Bliss",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "sofa",
+            "image": "asetshop/Sofa3.jpg",
+            "price": "Rp 15.000.000",
+            "title": "Modern Retreat Sofa",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kitchen",
+            "image": "asetshop/Dapur2.jpg",
+            "price": "Rp 30.000.000",
+            "title": "Culinary Heaven",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kursi",
+            "image": "asetshop/Kursi2.jpg",
+            "title": "Elegant Chair",
+            "price": "Rp 4.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 4,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "meja",
+            "image": "asetshop/Mejarias1.jpg",
+            "title": "Meja Rias Minimalis",
+            "price": "Rp 5.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "meja",
+            "image": "asetshop/Mejaeias2.jpg",
+            "title": "Meja Rias Grace",
+            "price": "Rp 6.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kitchen",
+            "image": "asetshop/dapur3.jpg",
+            "title": "Kitchen Corner",
+            "price": "Rp 29.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "meja",
+            "image": "asetshop/mejarias3jpg.jpg",
+            "title": "Meja Rias Serenity Glamour",
+            "price": "Rp 17.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "kitchen",
+            "image": "asetshop/dapur4.jpg",
+            "title": "Kitchen Savory",
+            "price": "Rp 16.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        },
+        {
+            "category": "meja",
+            "image": "asetshop/mejarias4.jpg",
+            "title": "Meja Rias Radiant Reflection",
+            "price": "Rp 8.000.000",
+            "description": "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "rating": 5,
+            "button": {
+                "text": "Add to Cart",
+                "icon": "fa fa-shopping-cart"
+            }
+        }
+    ]
+}
+
+function allProducts() {
+    var containerAllProducts = document.getElementById("all-product")
+
+    allProductsData.products.forEach(element => {
+        var rated = ""
+
+        for(let i=0; i<element.rating; i++) {
+            rated += `<span class="fa fa-star checked"></span>`
+        }
+        
+        var templated = `<div class="col-md-3 lemari">
+                      <div class="card mb-3">
+                          <img src="${element.image}" class="card-img-top" alt="Living room" width="200px">
+                          <div class="card-body">
+                              <h5 class="card-title text-center">${element.title}</h5>
+                              <p class="card-price text-center">${element.price}</p>
+                              <p class="card-text text-center">${element.description}</p>
+                              <div class="d-flex justify-content-between align-items-center mt-2">
+                              <div class="text-right">
+                                <!--Star Rating-->
+                                ${rated}
+                              </div>
+                              <div class="text-left">
+                                <!--Shopping Cart-->
+                                <button type="button" class="btn btn-outline-secondary"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                              </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>`
+                  containerAllProducts.innerHTML+=templated
+    });
+}
+
+function findByCategory() {
+    var kategori = document.getElementById("kategori").value
+    var containerAllProducts = document.getElementById("all-product")
+
+    var products = "";
+    allProductsData.products.forEach(element => {
+        if(element.category == kategori.toLowerCase()) {
+            var rated = ""
+
+            for(let i=0; i<element.rating; i++) {
+                rated += `<span class="fa fa-star checked"></span>`
+            }
+            
+            var templated = `<div class="col-md-3 lemari">
+                        <div class="card mb-3">
+                            <img src="${element.image}" class="card-img-top" alt="Living room" width="200px">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">${element.title}</h5>
+                                <p class="card-price text-center">${element.price}</p>
+                                <p class="card-text text-center">${element.description}</p>
+                                <div class="d-flex justify-content-between align-items-center mt-2">
+                                <div class="text-right">
+                                    <!--Star Rating-->
+                                    ${rated}
+                                </div>
+                                <div class="text-left">
+                                    <!--Shopping Cart-->
+                                    <button type="button" class="btn btn-outline-secondary"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+            products += templated    
+        }
+    });
+
+    containerAllProducts.innerHTML = products
+}
